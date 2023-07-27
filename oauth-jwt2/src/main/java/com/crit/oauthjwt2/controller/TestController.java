edc1af9be7aea9bcac860a0f55abb9d8db823534
rlvy98 <rlvy98@naver.com>
2023-07-27 10:12:00 +0900

    feat: JWT Filter 로직 수정

--- oauth-jwt2/src/main/java/com/crit/oauthjwt2/controller/TestController.java
+++ oauth-jwt2/src/main/java/com/crit/oauthjwt2/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.crit.oauthjwt2.controller;
 
+import com.crit.oauthjwt2.common.security.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
+import javax.servlet.http.HttpServletRequest;
 
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    private final SecurityUtil securityUtil;
     @GetMapping("/test")
-    public String getTest() {
+    public String getTest(HttpServletRequest httpServletRequest) {
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+        String token = authorizationHeader.substring(7);
+        String userId = (String) securityUtil.get(token).get("userId");
         System.out.println("===================testing=====================");
+        System.out.println("=========" + userId + "=============");
         return "success Test";
     }
 }
