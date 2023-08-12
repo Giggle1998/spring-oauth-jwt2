f84957611b112e27610430d217db4ce065225131
Giggle1998 <rlvy98@naver.com>
2023-08-12 16:00:04 +0900

    feat: 저장된 Redis RefreshToken 조회

---
+++ oauth-jwt2/src/main/java/com/crit/oauthjwt2/controller/RedisController.java
@@ -0,0 +1,31 @@
+package com.crit.oauthjwt2.controller;
+
+import com.crit.oauthjwt2.common.security.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class RedisController {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final SecurityUtil securityUtil;
+
+    @GetMapping("/redis")
+    public ResponseEntity<Object> getRedis(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization").substring(7);
+        String userId = (String)securityUtil.get(token).get("userId");
+        log.info(userId);
+        Object refreshToken = redisTemplate.opsForValue().get("RefreshToken:"+userId);
+        log.info(refreshToken.toString());
+        return ResponseEntity.ok(refreshToken);
+
+    }
+
+}
